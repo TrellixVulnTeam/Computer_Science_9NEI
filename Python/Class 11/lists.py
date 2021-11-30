d94fab964886d2b94ce68bfe560baa84b5d44b9c
Devansh-rookie <74622357+Devansh-rookie@users.noreply.github.com>
2021-12-01 01:09:54 +0530

    Save

--- Python/Class 11/lists.py
+++ Python/Class 11/lists.py
@@ -13,4 +13,75 @@
 
 # print(s)
 
+# list1 = [1,2,3,4,5]
+# a = len(list1)
+# j=0
+# for i in range(a):
+    # j+=i
+    # print(list1[i])
+# print(j)
+# 
+
+# '''Aliasing'''
+
+# list1 = [1,2,3,4,5]
+
+# list2 = list1
+
+# list2[0]=10
+# print(list1)
+# print(list2)
+
+# '''
+# Out put
+# [10, 2, 3, 4, 5]
+# [10, 2, 3, 4, 5]
+
+# '''
+
+# list1 = [1,2]
+# list2 =[3,4]
+
+# print(list1>list2)
+
+
+# list1 = [1,2]
+# list2 =[1,4]
+
+# print(list1>list2)
+
+
+# list1 = [10.4,2]
+# list2 =[10.3,2]
+
+# print(list1>list2)
+
+"""True"""
+
+# list1 = [1,2]
+# list2 =[3,4,2]
+
+# print(list1>list2)
+
+
+# list1 = [10,20,30]
+# list2 = [40,50,60]
+
+# # list3 = list1+ [100]
+
+# list3 = list1+ 100
+
+# print(list3)
+
+# list3 = list1+list2
+# print(list3)
+
+
+
+# list1 = [10,20,30]
+# list2 = [40,50,60]
+# list3 = list1*list2
+# print(list3)
+
+
 
